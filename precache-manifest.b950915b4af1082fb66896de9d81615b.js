9d5a0258baf64a83f67f0189d680142a1ee70885
Jordi Buj <jordi.buj@jamgo.coop>
2021-11-21 12:48:24 +0100

    Updates

---
+++ precache-manifest.b950915b4af1082fb66896de9d81615b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "00784ede36d5d9e102f6",
+    "url": "/bambes/static/css/main.ead3e1ea.chunk.css"
+  },
+  {
+    "revision": "00784ede36d5d9e102f6",
+    "url": "/bambes/static/js/main.00784ede.chunk.js"
+  },
+  {
+    "revision": "5a80405e8fff7c81f25f",
+    "url": "/bambes/static/js/1.5a80405e.chunk.js"
+  },
+  {
+    "revision": "f065bf213c905a9939d6",
+    "url": "/bambes/static/js/runtime~main.f065bf21.js"
+  },
+  {
+    "revision": "84287d09b8053c6fa598893b8910786a",
+    "url": "/bambes/static/media/logo.84287d09.svg"
+  },
+  {
+    "revision": "4cffa739f751f90686ec3ac6e9b56e0d",
+    "url": "/bambes/static/media/icon-check.4cffa739.svg"
+  },
+  {
+    "revision": "758e67590f3e1548b581949bc5e6aa6a",
+    "url": "/bambes/index.html"
+  }
+];
